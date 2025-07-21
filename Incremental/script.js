c94ac3aaf477acb9ec98d04c362c25d09141a2df
auselessguy2056 <auselessguy2056@gmail.com>
2025-07-21 09:17:52 +0700

    Update script.js

--- Incremental/script.js
+++ Incremental/script.js
@@ -2299,7 +2299,7 @@ const game = (() => {
             { question: "Tài nguyên cuối cùng trong Giai Đoạn 3 là gì?", answer: "đỉnh cao sáng tạo", reward: 250000 },
             { question: "Điều gì đến sau Đỉnh Cao Sáng Tạo?", answer: "điểm kỳ dị vũ trụ", reward: 300000 },
             { question: "Tài nguyên sau Điểm Kỳ Dị Vũ Trụ là gì?", answer: "vải tồn tại", reward: 350000 },
-            { question: "Tài nguyên sau Dòng Thời Gian là gì?", answer: "hòa hợp đa chiều", reward: 400000 },
+            { question: "Tài nguyên sau Dòng Chảy Thời Gian là gì?", answer: "hòa hợp đa chiều", reward: 400000 },
             { question: "Tài nguyên tối thượng của tất cả là gì?", answer: "Hình Thức Tối Thượng", reward: 500000 }
         ]
     };
